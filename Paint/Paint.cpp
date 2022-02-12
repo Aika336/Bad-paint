fdd3f07311e993d445a1c76e8e96b241c064a12f
Aika336 <Vladock1999Suka@gmail.com>
2022-02-12 03:59:36 +0300

    update

--- Paint/Paint.cpp
+++ Paint/Paint.cpp
@@ -1,17 +1,21 @@
 ﻿#include "SFML\Graphics.hpp"
 #include <vector>
 
+#define CLOSE window.close()
+#define MOUSEPOS sf::Mouse::getPosition()
+
 int main() {
     std::vector<sf::VertexArray> vertices;
     vertices.push_back(sf::VertexArray());
     vertices[0].setPrimitiveType(sf::LinesStrip);
+
     int lines_number = 0;
-    int locked = false;
+    bool locked = false;
 
     sf::Color curr_col = sf::Color::Black;
     sf::Vector2i last_Mouse_pos(0, 0);
 
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Paint", sf::Style::Close, sf::ContextSettings(0, 0, 0));
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Paint");
     window.setFramerateLimit(60);
 
     sf::Vector2f Border_Offset(-5, -25);
@@ -22,9 +26,9 @@ int main() {
 
             if (event.type == sf::Event::KeyPressed)
                 if (event.key.code == sf::Keyboard::Key::Escape)
-                    window.close();
+                    CLOSE;
             if (event.type == sf::Event::Closed)
-                window.close();
+                CLOSE;
 
             if (event.type == sf::Event::MouseButtonPressed) {
                 locked = true;
@@ -40,10 +44,10 @@ int main() {
         }
 
         if (locked) {
-            if (last_Mouse_pos != sf::Mouse::getPosition()) {
-                vertices[lines_number].append(sf::Vertex(sf::Vector2f(sf::Mouse::getPosition().x - window.getPosition().x + Border_Offset.x, sf::Mouse::getPosition().y - window.getPosition().y + Border_Offset.y), curr_col));
+            if (last_Mouse_pos != MOUSEPOS) {
+                vertices[lines_number].append(sf::Vertex(sf::Vector2f(MOUSEPOS.x - window.getPosition().x + Border_Offset.x, MOUSEPOS.y - window.getPosition().y + Border_Offset.y), curr_col));
 
-                last_Mouse_pos = sf::Mouse::getPosition();
+                last_Mouse_pos = MOUSEPOS;
             }
         }
 
@@ -52,10 +56,8 @@ int main() {
         for (int i = 0; i < vertices.size(); i++) {
             window.draw(vertices[i]);
         }
-
         window.display();
     }
-
 
     return 0;
 }
